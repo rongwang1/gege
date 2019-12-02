3b7cbd9060258eb5e3988430565d83ad661535dd
rongwang1 <614538673@qq.com>
2019-12-02 18:26:03 +0800

    03

--- src/pages/login/components/Header.vue
+++ src/pages/login/components/Header.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>this is header</div>
+ <div class="login-box">
+   <div class="login-content">
+      <div class="login-img">
+        <img src="@/assets/login.png" alt />
+      </div>
+   </div>
+   <div class="login-info"></div>
+ </div>
 </template>
 <script>
 export default {
   name: 'LoginHeader'
 }
 </script>
+<style lang="scss">
+</style>
