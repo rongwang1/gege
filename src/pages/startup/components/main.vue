b8aac34fcd1fa43fada84653efbc4f92bc59f8c5
rongwang1 <614538673@qq.com>
2019-12-05 18:25:52 +0800

    05

--- src/pages/startup/components/main.vue
+++ src/pages/startup/components/main.vue
@@ -1,14 +1,22 @@
 <template>
   <div class='startup'>
     <div class='bg-img'>
-      <img src="@/assets/login.png" />
+      风华无双
     </div>
   </div>
 </template>
 <style lang="scss">
-.startup{
-  position: relative;
-  width: 100%;
-  height:100%;
-}
+    .startup {
+      background: url('login.png');
+      background-size: cover;
+      background-repeat: no-repeat;
+      width: 1400px;
+      height: 1000px;
+      .bg-img{
+        font: bold 60px helvetica, arial, sans-serif;
+        color: rgb(210, 226, 233);
+        text-shadow: 0 0 0.2em  #F87,
+                     0 0 0.2em rgb(215, 226, 230);
+      }
+  }
 </style>
