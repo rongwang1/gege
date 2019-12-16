6a46093ae1828c1f5c0b58dbc31401b0307931f6
rongwang1 <614538673@qq.com>
2019-12-16 18:12:28 +0800

    10

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,16 +1,5 @@
 <template>
  <div class='homePage'>
-<<<<<<< HEAD
-     <div class="list">
-       <ul class="content-list">
-         <li><a href="#">首页</a></li>
-         <li><a href="#">作品展示</a></li>
-         <li><a href="#">直播</a></li>
-         <li><a href="#">安利</a></li>
-         <li><a href="#">联系</a></li>
-       </ul>
-     </div>
-=======
    <el-row>
      <el-col :span="16" :offset="4">
        <el-tabs v-model="activeName">
@@ -23,31 +12,31 @@
             <dl class='info-left'>
               <dt>中文名 </dt>
               <dd>风华无双</dd>
-              <dt>别名</dt>
+              <dt>别&nbsp;&nbsp;&nbsp;&nbsp;名</dt>
               <dd>格格</dd>
-              <dt>国籍</dt>
+              <dt>国&nbsp;&nbsp;&nbsp;&nbsp;籍</dt>
               <dd>中国</dd>
               <dt>出生地</dt>
               <dd>江西省九江市</dd>
               <dt>出生日期</dt>
               <dd>1987年10月20日</dd>
-              <dt>职业</dt>
+              <dt>职&nbsp;&nbsp;&nbsp;&nbsp;业</dt>
               <dd>配音演员</dd>
               <dt>代表作品</dt>
               <dd>青龙图腾、乱世为王、艳鬼、民国遗事1931、焉知非狐、暮云深、琅琊版、剑在天下、独闯天涯广播剧</dd>
             </dl>
             <dl class="info-right">
-              <dt>性别</dt>
+              <dt>性&nbsp;&nbsp;&nbsp;&nbsp;别</dt>
               <dd>男</dd>
-              <dt>星座</dt>
+              <dt>星&nbsp;&nbsp;&nbsp;&nbsp;座</dt>
               <dd>天秤座</dd>
               <dt>声音特色</dt>
               <dd>独一无二绝美音色（无脑吹）</dd>
-              <dt>爱好</dt>
+              <dt>爱&nbsp;&nbsp;&nbsp;&nbsp;好</dt>
               <dd>啃瓶子，找真爱</dd>
-              <dt>社团</dt>
+              <dt>社&nbsp;&nbsp;&nbsp;&nbsp;团</dt>
               <dd>怀旧配音联盟</dd>
-              <dt>身高</dt>
+              <dt>身&nbsp;&nbsp;&nbsp;&nbsp;高</dt>
               <dd>173</dd>
               <dt>现居地</dt>
               <dd>日本</dd>
@@ -55,17 +44,22 @@
           </div>
          </div>
         </el-tab-pane>
-        <el-tab-pane label="作品展示" name="second">作品展示</el-tab-pane>
+        <el-tab-pane label="作品展示" name="second">
+          <home-list></home-list>
+        </el-tab-pane>
         <el-tab-pane label="其他" name="third">其他</el-tab-pane >
        </el-tabs>
      </el-col>
    </el-row>
->>>>>>> fcd9f183fd4d3391343ebb5565254c0a4de58417
  </div>
 </template>
 <script>
+import HomeList from './List'
 export default {
   name: 'HomeHeader',
+  components: {
+    HomeList
+  },
   data () {
     return {
       activeName: 'first'
@@ -91,15 +85,41 @@ export default {
      }
     }
     .info{
-      width: 900px;
+      width: 88%;
       float: right;
       .info-left{
         display: inline-block;
-        width:440px;
+        width:50%;
+        dt{
+          width: 60px;
+          float: left;
+          font-weight: 700;
+          color: #918585;
+          margin-right: 20px;
+          line-height: 20px;;
+        }
+         dd{
+            width: 350px;
+            margin-left:40px;
+            line-height: 20px;
+        }
       }
       .info-right{
         display: inline-block;
-        width:440px;                                                                                                                                                                                                                     
+        width:44%;
+        margin-left: 40px;
+        dt{
+          width: 60px;
+          font-weight: 700;
+          color: #918585;
+          float: left;
+          margin-right: 20px;
+        }
+        dd{
+          width: 350px;
+          margin-left: 40px;
+          line-height: 20px;
+        }                                                                                                                               
       }
     }
   }
---
+++ src/pages/home/components/List.vue
@@ -0,0 +1,45 @@
+<template>
+ <div>
+   <el-carousel :interval="4000" type="card" height="200px">
+       <el-carousel-item v-for='item of swiperList' :key="item.id">
+          <img  class="banner-img" :src="item.url" alt="#">
+       </el-carousel-item>
+   </el-carousel>
+ </div>
+</template>
+<script>
+export default {
+  name: 'HomeList',
+  data () {
+    return {
+      swiperList: [{
+        id: '001',
+        url: 'http://bpic.51yuansu.com/banner/banner19-shengdan.png'
+      },
+      {
+        id: '002',
+        url: 'http://bpic.51yuansu.com/banner/banner19-q4.jpg'
+      },
+      {
+        id: '003',
+        url: 'http://bpic.51yuansu.com/banner/banner20-yuandan.png'
+      },
+      {
+        id: '004',
+        url: 'http://bpic.51yuansu.com/banner/banner19-shengdan.png'
+      },
+      {
+        id: '005',
+        url: 'http://pic.51yuansu.com/banner/banner18-onlinebj.png'
+      },
+      {
+        id: '006',
+        url: 'http://bpic.51yuansu.com/activity/20191202/5de4c09f70f0a.png'
+      }]
+    }
+  }
+}
+</script>
+<style lang="scss">
+</style>
+
